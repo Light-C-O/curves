cca00b501b660c34299f8df55be21af9fa078698
Light <N00242379@iadt.ie>
2026-03-20 15:12:32 +0000

    attempting separating

---
+++ part_one/Curve.js
@@ -0,0 +1,63 @@
+class Curve {
+    constructor (){
+        this.stepSize = 2;
+        this.distortionFactor = 1;
+        this.centerX;
+        this.centerY;
+        this.x = [];
+        this.y = [];
+
+        this.filled = false;
+        this.freeze = false;
+        this.drawMode = 1;
+    }
+    render (){
+        // floating towards mouse position
+        this.centerX += (mouseX - centerX) * 0.01;
+        this.centerY += (mouseY - centerY) * 0.01;
+
+        // calculate new points
+        for (let i = 0; i < this.formResolution; i++) {
+            x[i] += random(-this.stepSize, this.stepSize);
+            y[i] += random(-this.stepSize, this.stepSize);
+        }
+
+        if (this.filled) {
+            fill(random(255));
+        } else {
+            noFill();
+        }
+
+        switch (drawMode) {
+        case 1: // circle
+            beginShape();
+            // start controlpoint
+            curveVertex(x[this.formResolution - 1] + this.centerX, y[this.formResolution - 1] + this.centerY);
+
+            // only these points are drawn
+            for (let i = 0; i < this.formResolution; i++) {
+            curveVertex(x[i] + this.centerX, y[i] + this.centerY);
+            }
+            curveVertex(x[0] + this.centerX, y[0] + this.centerY);
+
+            // end controlpoint
+            curveVertex(x[1] + this.centerX, y[1] + this.centerY);
+            endShape();
+            break;
+        case 2: // line
+            beginShape();
+            // start controlpoint
+            curveVertex(x[0] + this.centerX, y[0] + this.centerY);
+
+            // only these points are drawn
+            for (let i = 0; i < this.formResolution; i++) {
+            curveVertex(x[i] + this.centerX, y[i] + this.centerY);
+            }
+
+            // end controlpoint
+            curveVertex(x[this.formResolution - 1] + this.centerX, y[this.formResolution - 1] + this.centerY);
+            endShape();
+            break;
+        }
+    }
+}
