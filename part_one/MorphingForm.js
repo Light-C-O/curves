cafdc78ca2e58013de07a7146e36fe3dd8b76f94
Light-C-O <N00242379@iadt.ie>
2026-03-26 10:37:56 +0000

    completed in refactoring code into OOP structure

--- part_one/MorphingForm.js
+++ part_one/MorphingForm.js
@@ -1,73 +1,125 @@
 class MorphingForm {
-    //what it remembers
-    constructor (){
+    //what it remembers - runs once 
+    constructor (_centerX, _centerY){
         this.formResolution = 15;
         this.initRadius = 150;
-        this.stepSize = 2;
-        this.centerX;
-        this.centerY;
+        this.centerX = _centerX;
+        this.centerY = _centerY;
         this.x = [];
         this.y = [];
-
         this.filled = false;
-        this.freeze = false;
         this.drawMode = 1;
+        this.stepSize = 2;
+
+        let angle = radians(360 / this.formResolution);
+        for (let i = 0; i < this.formResolution; i++) {
+            this.x.push(cos(angle * i) * this.initRadius);
+            this.y.push(sin(angle * i) * this.initRadius);
+        }
     }
 
     //methods - what it does
-    
     render (){
-       
+        this.update();
+        this.draw();
     }
 
-
-    draw(){
-         // floating towards mouse position
-        this.centerX += (mouseX - centerX) * 0.01;
-        this.centerY += (mouseY - centerY) * 0.01;
+    //for the mouse position to move
+    update(){
+        // floating towards mouse position
+        this.centerX += (mouseX - this.centerX) * 0.01;
+        this.centerY += (mouseY - this.centerY) * 0.01;
 
         // calculate new points
         for (let i = 0; i < this.formResolution; i++) {
-            x[i] += random(-this.stepSize, this.stepSize);
-            y[i] += random(-this.stepSize, this.stepSize);
+            this.x[i] += random(-this.stepSize, this.stepSize);
+            this.y[i] += random(-this.stepSize, this.stepSize);
         }
+    }
 
+    //to make the shapes
+    draw(){
         if (this.filled) {
             fill(random(255));
         } else {
             noFill();
         }
 
-        switch (drawMode) {
+        switch (this.drawMode) {
         case 1: // circle
             beginShape();
             // start controlpoint
-            curveVertex(x[this.formResolution - 1] + this.centerX, y[this.formResolution - 1] + this.centerY);
+            curveVertex(this.x[this.formResolution - 1] + this.centerX, this.y[this.formResolution - 1] + this.centerY);
 
             // only these points are drawn
             for (let i = 0; i < this.formResolution; i++) {
-            curveVertex(x[i] + this.centerX, y[i] + this.centerY);
+            curveVertex(this.x[i] + this.centerX, this.y[i] + this.centerY);
             }
-            curveVertex(x[0] + this.centerX, y[0] + this.centerY);
+            curveVertex(this.x[0] + this.centerX, this.y[0] + this.centerY);
 
             // end controlpoint
-            curveVertex(x[1] + this.centerX, y[1] + this.centerY);
+            curveVertex(this.x[1] + this.centerX, this.y[1] + this.centerY);
             endShape();
             break;
+
         case 2: // line
             beginShape();
             // start controlpoint
-            curveVertex(x[0] + this.centerX, y[0] + this.centerY);
+            curveVertex(this.x[0] + this.centerX, this.y[0] + this.centerY);
 
             // only these points are drawn
             for (let i = 0; i < this.formResolution; i++) {
-            curveVertex(x[i] + this.centerX, y[i] + this.centerY);
+            curveVertex(this.x[i] + this.centerX, this.y[i] + this.centerY);
             }
 
             // end controlpoint
-            curveVertex(x[this.formResolution - 1] + this.centerX, y[this.formResolution - 1] + this.centerY);
+            curveVertex(this.x[this.formResolution - 1] + this.centerX, this.y[this.formResolution - 1] + this.centerY);
             endShape();
             break;
+        }
+    }
+
+    //gets called when the user clicks the mouse
+    reset(_centerX, _centerY){
+        //needs to know the new center position
+        this.centerX = _centerX;
+        this.centerY = _centerY;
+
+        // to empty an array
+        this.x = []
+        this.y = []
+
+        let angle;
+        let radius;
+        
+
+    switch (this.drawMode) {
+    case 1: // circle
+
+        angle = radians(360 / this.formResolution);
+        radius = this.initRadius * random(0.5, 1);
+
+        for (let i = 0; i < this.formResolution; i++) {
+            this.x.push(cos(angle * i) * radius);
+            this.y.push(sin(angle * i) * radius);
+        }
+        break;
+
+    case 2: // line
+        radius = this.initRadius * random(0.5, 5);
+        angle = random(PI);
+
+        let x1 = cos(angle) * radius;
+        let y1 = sin(angle) * radius;
+        let x2 = cos(angle - PI) * radius;
+        let y2 = sin(angle - PI) * radius;
+
+        for (let i = 0; i < this.formResolution; i++) {
+            this.x.push(lerp(x1, x2, i / this.formResolution));
+            this.y.push(lerp(y1, y2, i / this.formResolution));
+        }
+        break;
+
         }
     }
 }
--- part_one/sketch.js
+++ part_one/sketch.js
@@ -35,139 +35,42 @@
  */
 'use strict';
 
-let formResolution = 15;
-let stepSize = 2;
+
 let distortionFactor = 1;
-let initRadius = 150;
-let centerX;
-let centerY;
-let x = [];
-let y = [];
 
-let filled = false;
 let freeze = false;
-let drawMode = 1;
 
-let curve;
+let form;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // init shape
-  centerX = width / 2;
-  centerY = height / 2;
-  let angle = radians(360 / formResolution);
-  for (let i = 0; i < formResolution; i++) {
-    x.push(cos(angle * i) * initRadius);
-    y.push(sin(angle * i) * initRadius);
-  }
-
   stroke(0, 50);
   strokeWeight(0.75);
   background(255);
+  form = new MorphingForm(width/2, height/2)
 
-  // noLoop()
 }
 
 function draw() {
-  // floating towards mouse position
-  centerX += (mouseX - centerX) * 0.01;
-  centerY += (mouseY - centerY) * 0.01;
-
-  // calculate new points
-  //for every loop it moves the points at random on the x(right, left)axis and on the y(up, down)axis
-  for (let i = 0; i < formResolution; i++) {
-    x[i] += random(-stepSize, stepSize);
-    y[i] += random(-stepSize, stepSize);
-  }
-
-  if (filled) {
-    fill(random(255));
-  } else {
-    noFill();
-  }
-
-  switch (drawMode) {
-  case 1: // circle
-    beginShape();
-    // start controlpoint
-
-    //use the last point as an extra curveVertex
-    //how curveVetex works is by looking at the neighbours because it needs to know where it came from.
-    //so point 14(ghost point) is important inorder for it to curve. We give the last point before we start the shape
-    //the + centerX shifts the point to the right position
-    curveVertex(x[formResolution - 1] + centerX, y[formResolution - 1] + centerY);
-
-    // only these points are drawn
-    for (let i = 0; i < formResolution; i++) {
-      curveVertex(x[i] + centerX, y[i] + centerY);
-    }
-    curveVertex(x[0] + centerX, y[0] + centerY);
-
-    // end controlpoint
-    curveVertex(x[1] + centerX, y[1] + centerY);
-    endShape();
-    break;
-  case 2: // line
-    beginShape();
-    // start controlpoint
-    curveVertex(x[0] + centerX, y[0] + centerY);
-
-    // only these points are drawn
-    for (let i = 0; i < formResolution; i++) {
-      curveVertex(x[i] + centerX, y[i] + centerY);
-    }
-
-    // end controlpoint
-    curveVertex(x[formResolution - 1] + centerX, y[formResolution - 1] + centerY);
-    endShape();
-    break;
-  }
+  form.render()
 }
 
 function mousePressed() {
-  // init shape on mouse position
-  centerX = mouseX;
-  centerY = mouseY;
-
-  let angle;
-  let radius;
-  switch (drawMode) {
-  case 1: // circle
-    angle = radians(360 / formResolution);
-    radius = initRadius * random(0.5, 1);
-    for (let i = 0; i < formResolution; i++) {
-      x[i] = cos(angle * i) * radius;
-      y[i] = sin(angle * i) * radius;
-    }
-    break;
-  case 2: // line
-    radius = initRadius * random(0.5, 5);
-    angle = random(PI);
-
-    let x1 = cos(angle) * radius;
-    let y1 = sin(angle) * radius;
-    let x2 = cos(angle - PI) * radius;
-    let y2 = sin(angle - PI) * radius;
-    for (let i = 0; i < formResolution; i++) {
-      x[i] = lerp(x1, x2, i / formResolution);
-      y[i] = lerp(y1, y2, i / formResolution);
-    }
-    break;
-  }
+  form.reset(mouseX, mouseY)
 }
 
 function keyReleased() {
   if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
   if (keyCode == DELETE || keyCode == BACKSPACE) background(255);
-  if (key == '1') filled = false;
-  if (key == '2') filled = true;
-  if (key == '3') drawMode = 1;
-  if (key == '4') drawMode = 2;
+  if (key == '1') form.filled = false;
+  if (key == '2') form.filled = true
+  if (key == '3') form.drawMode = 1;
+  if (key == '4') form.drawMode = 2;
 
-  if (keyCode == UP_ARROW) stepSize++;
-  if (keyCode == DOWN_ARROW) stepSize--;
-  stepSize = max(stepSize, 1);
+  if (keyCode == UP_ARROW) form.stepSize++;
+  if (keyCode == DOWN_ARROW) form.stepSize--;
+  form.stepSize = max(form.stepSize, 1);
 
   // pause/play draw loop
   if (key == 'f' || key == 'F') freeze = !freeze;
